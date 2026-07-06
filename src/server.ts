301c917b3a7e5da7b0cd3b752ea38cbc398c1c05
mofazzal-hossen <mofazzal.hossen.official0@gmail.com>
2026-07-07 00:35:31 +0600

    refactor: improve code formatting and structure in server setup

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,17 @@
 import express, { type Application, type Request, type Response } from 'express'
-import {DatabaseError, Pool} from "pg"
-const app : Application = express()
+import { DatabaseError, Pool } from "pg"
+const app: Application = express()
 const port = 9000
 // you must used all time MeddleWare 
 app.use(express.json())
 app.use(express.text()) //show text data 
-app.use(express.urlencoded({extended:true})) //{extended:true} all type data show as like nested-data. 
+app.use(express.urlencoded({ extended: true })) //{extended:true} all type data show as like nested-data. 
 //end
 
 //pg connection 
 
 const pool = new Pool({
-  connectionString:"postgresql://neondb_owner:npg_hS6D7qkWBouK@ep-calm-leaf-atx7sci5-pooler.c-9.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+  connectionString: "postgresql://neondb_owner:npg_hS6D7qkWBouK@ep-calm-leaf-atx7sci5-pooler.c-9.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
 
 })
 
@@ -19,7 +19,7 @@ const pool = new Pool({
 //check data database connection
 //postgresql create tables search in w3 school
 
-const initDB = async()=>{
+const initDB = async () => {
   try {
     await pool.query(`
 CREATE TABLE IF NOT EXISTS users (
@@ -38,7 +38,7 @@ updated_at TIMESTAMP DEFAULT NOW()
 
       `)
 
-      console.log("DataBase connected successfully " )
+    console.log("DataBase connected successfully ")
   } catch (error) {
     console.log(error)
   }
@@ -47,31 +47,68 @@ initDB();
 //end
 
 
-app.get('/', (req :Request, res:Response) => {
+app.get('/', (req: Request, res: Response) => {
   // res.send('Hello World')
   res.status(200).json({
-    "message":"Express Level",
-    "aut":"next level"
+    "message": "Express Level",
+    "aut": "next level"
   })
 });
 
 ///API response-কে সুন্দর ও organized রাখার জন্য। , এভাবে success, message, data আলাদা থাকে এবং frontend-এর জন্য parse করা সহজ হয়।
 // you must be check INSERT query-তে parameter-এর order 
-app.post('/', async(req:Request, res:Response)=>{
+app.post('/api/user', async (req: Request, res: Response) => {
   // console.log(req.body) you do try clg
 
-  const {name , email,age, password}= req.body   
-  const result = await pool.query(`
+  const { name, email, age, password } = req.body
+
+  try {
+    const result = await pool.query(`
     INSERT INTO users (name, email,  password ,age  )  
     VALUES ($1,$2,$3,$4) RETURNING *
     
-    `,[name , email, password, age ])
-  res.status(201).json({
-    message:"successfully make by data",
-    data: result.rows[0],
+    `, [name, email, password, age])
+
+    res.status(201).json({
+      message: "successfully make by data",
+      data: result.rows[0],
 
 
     })
+  } catch (error: any) {
+    res.status(500).json({
+      message: error.message,
+      error: error,
+
+
+    })
+
+  }
+});
+
+
+
+app.get('/api/user', async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(`
+    SELECT * FROM users
+`)
+
+res.status(200).json({
+  success:true,
+  message:"user successfully red " ,
+  data: result.rows,
+
+})
+  } catch (error:any) {
+res.status(500).json({
+    success:false,
+  message:error.message,
+  error:error,
+
+})
+
+  }
 })
 
 
